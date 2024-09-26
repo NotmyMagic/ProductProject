0fab5f02f8d408670bb363095f937e7175fb6800
NotmyMagic <56935337+NotmyMagic@users.noreply.github.com>
2024-09-26 14:01:28 -0600

    working on js

---
+++ test.js
@@ -0,0 +1,19 @@
+productArray = [
+  "skateboard",
+  "freeskates",
+  "griptape",
+  "scooter",
+  "rollerskates",
+  "rollerblades",
+  "grindwax",
+];
+console.log(productArray);
+
+const output = document.querySelector(".output");
+const product = document.querySelector("#product");
+
+function addProduct() {
+  if (input) {
+  } else {
+  }
+}
